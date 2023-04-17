4b856d94a127b7a5f50971775d7bca6aa16b6198
Luciano Angeleri <lcnang@gmail.com>
2023-04-17 04:11:37 -0300

    Se agregó la función para mostrar la hora

--- js/app.js
+++ js/app.js
@@ -2,9 +2,27 @@ let fechaActual = new Date(),
     diaNum = fechaActual.getDate(),
     diaSemana = fechaActual.getDay(),
     mes = fechaActual.getMonth(),
-    anio = fechaActual.getFullYear();
+    anio = fechaActual.getFullYear(),
+    hs = fechaActual.getHours(),
+    min = fechaActual.getMinutes(),
+    seg = fechaActual.getSeconds(),
+    siglas = "AM";
 const nombresDias= ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
 const nombresMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-let textoFecha = document.getElementById("textoFecha")
+let textoFecha = document.getElementById("textoFecha"),
+    textoHs = document.getElementById("horas"),
+    textoMin = document.getElementById("minutos"),
+    textoSeg = document.getElementById("segundos"),
+    textoSiglas = document.getElementById("siglas");
+
 textoFecha.innerHTML=`${nombresDias[diaSemana]} ${diaNum} de ${nombresMeses[mes]} del ${anio}`
+
+if (hs >= 12) {
+    hs -=12;
+    siglas = "PM";
+}
+textoHs.innerHTML = `${hs}<small class="fs-6 ps-1">hs</small>`
+textoMin.innerHTML = `${min}<small class="fs-6 ps-1">min</small>`
+textoSeg.innerHTML = `${seg}<small class="fs-6 ps-1">seg</small>`
+textoSiglas.innerHTML = `${siglas}`
