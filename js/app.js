9f9a220128d2d52e734b38590ac9c1b5376995bd
Luciano Angeleri <lcnang@gmail.com>
2023-04-17 06:05:35 -0300

    Se ajustó para que quede responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-let fechaActual = new Date(),
+setInterval(() => {
+  let fechaActual = new Date(),
     diaNum = fechaActual.getDate(),
     diaSemana = fechaActual.getDay(),
     mes = fechaActual.getMonth(),
@@ -7,36 +8,50 @@ let fechaActual = new Date(),
     min = fechaActual.getMinutes(),
     seg = fechaActual.getSeconds(),
     siglas = "AM";
-const nombresDias= ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-const nombresMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+  const nombresDias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+  const nombresMeses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
 
-let textoFecha = document.getElementById("textoFecha"),
+  let textoFecha = document.getElementById("textoFecha"),
     textoHs = document.getElementById("horas"),
     textoMin = document.getElementById("minutos"),
     textoSeg = document.getElementById("segundos"),
     textoSiglas = document.getElementById("siglas");
 
-textoFecha.innerHTML=`${nombresDias[diaSemana]} ${diaNum} de ${nombresMeses[mes]} del ${anio}`
+  textoFecha.innerHTML = `${nombresDias[diaSemana]} ${diaNum} de ${nombresMeses[mes]} del ${anio}`;
 
-if (hs >= 12) {
-    hs -=12;
+  if (hs >= 12) {
+    hs -= 12;
     siglas = "PM";
-}else{
+  } else {
     siglas = "AM";
-}
-if (hs == 0) {
+  }
+  if (hs == 0) {
     hs = 12;
-}
-if (hs < 10) {
+  }
+  if (hs < 10) {
     hs = "0" + hs;
-}
-if (min < 10) {
+  }
+  if (min < 10) {
     min = "0" + min;
-}
-if (seg < 10) {
+  }
+  if (seg < 10) {
     seg = "0" + seg;
-}
-textoHs.innerHTML = `${hs}<small class="fs-6 ps-1">hs</small>`
-textoMin.innerHTML = `${min}<small class="fs-6 ps-1">min</small>`
-textoSeg.innerHTML = `${seg}<small class="fs-6 ps-1">seg</small>`
-textoSiglas.innerHTML = `${siglas}`
+  }
+  textoHs.innerHTML = `${hs}<small class="fs-6 ps-1 d-none d-md-inline">hs</small>`;
+  textoMin.innerHTML = `${min}<small class="fs-6 ps-1 d-none d-md-inline">min</small>`;
+  textoSeg.innerHTML = `${seg}<small class="fs-6 ps-1 d-none d-md-inline">seg</small>`;
+  textoSiglas.innerHTML = `${siglas}`;
+}, 1000);
